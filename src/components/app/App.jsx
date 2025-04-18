5e1c7f07cee196bbf2243fbb2d9c064ce0c05a0f
unknown <voductaitxqt123@mail.com>
2025-04-18 13:25:54 +0700

    done a camera component

---
+++ src/components/app/App.jsx
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+// import ReactDOM from 'react-dom/client';
+import CameraShop from '../cameraShop/CameraShop.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <CameraShop/>
+    </div>
+  );
+}
+
+export default App;
